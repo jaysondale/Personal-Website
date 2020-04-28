d6a41a6128c3615538df075a1a071cd940924aa6
jaysondale <jaysondale1@gmail.com>
2020-04-28 15:41:27 -0400

    Added experience, education, and projects sections

--- js/main.js
+++ js/main.js
@@ -23,8 +23,9 @@
     };
 
     TextRotation.prototype.rotate = function() {
-        var current = this.loopCount % this.texts.length;
-        this.element.innerHTML = this.texts[current];
+        let current = this.loopCount % this.texts.length;
+
+        this.element.innerHTML = '<span class="fadeIn ftco-animated">' +  this.texts[current] + '</span>';
 
         // Increment loop count
         this.loopCount += 1;
@@ -42,7 +43,65 @@
                 new TextRotation(elements[i], texts, period);
             }
         }
-    }
+    };
+
+    // Scroll to page location (smoother transition)
+    let scrollToPage = function() {
+        $(document).on('click', '#ftco-nav a[href^="#"]', function(eventObject){
+            eventObject.preventDefault();
+            let link = $.attr(this, 'href');
+            $('html, body').animate({
+                scrollTop: $(link).offset().top - 70
+            }, 500);
+        });
+    };
+
+    scrollToPage();
+
+    // Scroll menu bar
+    let scrollWindow = function(){
+        $(window).scroll(function() {
+            let $win = $(this);
+            let pos = $win.scrollTop();
+            let navbar = $('.ftco-navbar');
+            if (pos > 150) {
+                if (!navbar.hasClass('scrolled')){
+                    navbar.addClass('scrolled');
+                }
+            }
+            if (pos < 150) {
+                if (navbar.hasClass('scrolled')){
+                    navbar.removeClass('scrolled');
+                }
+            }
+            if (pos > 350) {
+                if (!navbar.hasClass('awake')){
+                    navbar.addClass('awake');
+                }
+            }
+        })
+    };
+
+    scrollWindow();
+
+    let contentWayPoint = function() {
+        $('.ftco-animate').waypoint( function(direction) {
+            if (direction == 'down' && !$(this.element).hasClass('ftco-animated')) {
+                $(this.element).addClass('item-animate');
+                setTimeout(function() {
+                    $('body .ftco-animate.item-animate').each(function(k){
+                        let element = $(this);
+                        setTimeout(function() {
+                            element.addClass('fadeInUp ftco-animated');
+                            element.removeClass('item-animate')
+                        }, k * 50, 'easeInOutExpo')
+                    })
+                }, 100);
+            }
+        }, {offset: '95%'});
+    };
+
+    contentWayPoint();
 
 
 
